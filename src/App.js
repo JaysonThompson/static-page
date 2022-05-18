b9d5a8b16dfa314c6816ebd7eae1803e4dd71695
Jayson Thompson <jaysonthompson922@gmail.com>
2022-05-17 23:20:35 -0700

    Added my project

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const App = () => {
+  return (
+    <>
+      <img src="public/logo192.png" alt="React Logo" width="40px" />
+      <h1>Fun facts about React</h1>
+      <ul>
+        <li>Was first released in 2013</li>
+        <li>Was originally created bby Jorday Walke</li>
+        <li>Has well over 100K stars in GitHub</li>
+        <li>Is maintained by Facebook</li>
+        <li>Powers thousands of enterprise apps, including mobile apps</li>
+      </ul>
+    </>
+  )
+}
+
+export default App
