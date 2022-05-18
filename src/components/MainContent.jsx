e84288ab86dc7636706cb3dd59407f4ae4a9ddfb
Jayson Thompson <jaysonthompson922@gmail.com>
2022-05-18 10:06:14 -0700

    Creat MainContent component and adds it to the App.js file

---
+++ src/components/MainContent.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const MainContent = () => {
+  return (
+    <>
+      <h1>Jayson's React Page</h1>
+      <ol>
+        <li>Item 1</li>
+        <li>Item 2</li>
+        <li>Item 3</li>
+        <li>Item 4</li>
+        <li>Item 5</li>
+      </ol>
+    </>
+  )
+}
+export default MainContent
